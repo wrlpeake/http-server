bd109a5b41f0c7e723c390e84f4119dba73cf48b
wrlpeake <wrlpeake@gmail.com>
2023-01-13 15:51:39 +0000

    Tweaked test cases to improve quality.

--- src/test/java/http/RequestHandlerTest.java
+++ src/test/java/http/RequestHandlerTest.java
@@ -11,21 +11,21 @@ public class RequestHandlerTest {
 
     @Test
     public void getRequestTest() throws IOException {
-        String request = "GET /simple_get HTTP/1.1\r\n";
+        String request = "OPTIONS /method_options2 HTTP/1.1\r\n";
         ByteArrayInputStream input = new ByteArrayInputStream(request.getBytes());
 
         assertEquals(request, RequestHandler.getRequest(input));
     }
     @Test
     public void getRequestParametersTest() {
-        String request = "GET /simple_get HTTP/1.1\r\n";
-        String parameters = "GET /simple_get HTTP/1.1";
+        String request = "POST /echo_body HTTP/1.1\r\nContent-Type: text/plain\r\nContent-Length: 7\r\n\r\nSunrise";
+        String parameters = "POST /echo_body HTTP/1.1";
         assertEquals(parameters, RequestHandler.getRequestParameters(request));
     }
 
     @Test
     public void getMethodTest() {
-        String request = "GET /simple_get HTTP/1.1\r\n";
+        String request = "GET /foo_bar HTTP/1.1\r\n";
         String parameters = RequestHandler.getRequestParameters(request);
         String method = "GET";
         assertEquals(method, RequestHandler.getMethod(parameters));
@@ -33,15 +33,15 @@ public class RequestHandlerTest {
 
     @Test
     public void getPathTest() {
-        String request = "GET /simple_get HTTP/1.1\r\n";
+        String request = "HEAD /under_the_sea HTTP/1.1\r\n";
         String parameters = RequestHandler.getRequestParameters(request);
-        String path = "/simple_get";
+        String path = "/under_the_sea";
         assertEquals(path, RequestHandler.getPath(parameters));
     }
 
     @Test
     public void getBodyTest() {
-        String request = "GET /echo_body HTTP/1.1\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello world";
+        String request = "POST /to_the_moon HTTP/1.1\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello world";
         String expectedBody = "Hello world";
         assertEquals(expectedBody, RequestHandler.getBody(request));
     }
